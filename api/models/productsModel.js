f0768ea4565d484cd8c9c2ceea4e3932293c56f6
Lilian Mathu <lilianmathu2018@gmail.com>
2022-10-23 23:17:19 +0300

    Add product model

---
+++ api/models/productsModel.js
@@ -0,0 +1,22 @@
+import { Schema, model } from "mongoose";
+
+const productsSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+  {
+    collection: "Products",
+  }
+);
+
+const ProductsModel = model("Products", productsSchema);
