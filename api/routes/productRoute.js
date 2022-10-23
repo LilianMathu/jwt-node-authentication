38924229be98d300dc288e8c3f77072c085cac6a
Lilian Mathu <lilianmathu2018@gmail.com>
2022-10-23 23:11:18 +0300

    Add product routes

---
+++ api/routes/productRoute.js
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import bodyParser from "body-parser";
+import morgan from "morgan";
+import productController from "../controllers/productController";
+import authMiddleware from "../middleware/checkAuth";
+
+const router = Router();
+
+// Mount
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use(morgan("dev"));
+
+// Product Routes
+router.post("/api/products", authMiddleware, productController.addNew);
+
+export default router;
