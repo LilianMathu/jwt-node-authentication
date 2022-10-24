2fba02c8f5fc53de9d4a4917c27fb90082da2d98
Lilian Mathu <lilianmathu2018@gmail.com>
2022-10-24 11:08:24 +0300

    Modify jwt verify middleware

--- api/middleware/checkAuth.js
+++ api/middleware/checkAuth.js
@@ -2,20 +2,19 @@ import jwt from "jsonwebtoken";
 import config from "../../config";
 
 const authMiddleware = (req, res, next) => {
-  try {
-    const token = req.headers.authorization.split(" ")[1];
+  const token = req.headers.authorization.split(" ")[1];
 
-    const decoded = jwt.verify(token, config.secret);
-
-    // req.userData = decoded;
-
-    res.json({
-      message: "Successful!",
+  if (!token) {
+    res.status(403).json({ message: "No token was provided!" });
+  } else {
+    jwt.verify(token, config.secret, function (err, decoded) {
+      if (err) {
+        return res.json({ message: "Failed to authenticate token!" });
+      } else {
+        req.decoded = decoded;
+        next();
+      }
     });
-
-    next();
-  } catch (error) {
-    return res.status(401).json({ message: "Auth failed!" });
   }
 };
 
